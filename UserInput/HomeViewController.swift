ff15115a45ff52bc7ae983dea5c61f15ee1df3e0
Jason Valencius Wijaya <jasonwijaya097@gmail.com>
2019-05-16 15:52:44 +0700

    UITextField and NavigationController

---
+++ UserInput/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  UserInput
+//
+//  Created by Jason Valencius Wijaya on 16/05/19.
+//  Copyright © 2019 Jason Valencius Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- UserInput/ViewController.swift
+++ UserInput/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fullNameLabel: UITextField!
+    @IBOutlet weak var numberLabel: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //Buat assign delegate dari viewcontroller ke textfield
+        fullNameLabel.delegate = self
+        numberLabel.delegate = self
     }
 
 
+}
+
+//Extension buat TextField implementasi TextField Delegate
+extension ViewController: UITextFieldDelegate{
+    
+    //Kalo misalnya kita klik di keyboard return apa yang terjadi
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool{
+        if textField == fullNameLabel{
+            numberLabel.becomeFirstResponder()
+        }else if textField == numberLabel{
+            //Menghilangkan fokus dari textfield yang sedang fokus
+            textField.resignFirstResponder()
+        }
+        
+        return true
+    }
 }
 
