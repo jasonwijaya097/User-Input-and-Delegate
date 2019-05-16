b146cae66153d7f2901afa643cc50e560c828902
Jason Valencius Wijaya <jasonwijaya097@gmail.com>
2019-05-16 15:21:46 +0700

    Initial Commit

---
+++ UserInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInput
+//
+//  Created by Jason Valencius Wijaya on 16/05/19.
+//  Copyright © 2019 Jason Valencius Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
